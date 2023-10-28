ec890c117c5dac526bc47a3b1cc06a61cf194617
kolyakhmarjuk@gmail.com <kolyakhmarjuk@gmail.com>
2023-10-28 18:24:07 +0300

    hw3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts/index");
-
 const { addSchema, updateFavoriteSchema } = require("../../models/contacts");
 const { validationBody, isValidId } = require("../../middlewares");
 
